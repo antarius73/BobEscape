dc344827a99b832f56a13cb07088cce3529dabd5
antarius73 <gjonik@gmail.com>
2016-11-22 17:26:47 +0100

    mise en place de Messenger

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEvent {
+	public const string PLAYER_TURN_START = "PLAYER_TURN_START";
+	public const string PLAYER_MOVE_START = "PLAYER_MOVE_START";
+	public const string PLAYER_MOVE_END = "PLAYER_MOVE_END";
+	public const string PLAYER_TURN_END = "PLAYER_TURN_END";
+}
